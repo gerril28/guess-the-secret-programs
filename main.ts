7d2b15faea6c637d0a1c8ee78387c27feb28bd9d
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-10-03 09:32:12 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,2 +1,24 @@
-let number = 0
-number += randint(0, 10)
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Number2)
+    Number2 += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    let Guess = 0
+    if (Guess == Number2) {
+        basic.showString("CORRECT")
+        basic.showIcon(IconNames.Yes)
+    } else if (Guess == Number2) {
+        basic.showString("INCORRECT")
+        basic.showIcon(IconNames.No)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Number2)
+    Number2 += -1
+})
+input.onGesture(Gesture.Shake, function () {
+    control.reset()
+})
+let Number2 = 0
+basic.showString("GTSP")
+Number2 = randint(0, 20)
